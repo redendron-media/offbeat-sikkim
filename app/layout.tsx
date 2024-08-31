19c5c7ca0e4818670bdb0a545e8ce718cd140297
Ramit <ramitsingh12@gmail.com>
2024-08-31 19:09:35 +0530

    facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme";
 import Script from "next/script";
-import SmoothScroll from "@/components/scroll-component/page";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "700"],
@@ -49,13 +48,37 @@ export default function RootLayout({
           <CssBaseline />
        
           <Header />
-          <SmoothScroll>
+  
           {children}
-          </SmoothScroll>   
+     
           <Footer />
         </ThemeProvider>   
 
-   
+       <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+      >
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '8094359647352078');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=id=8094359647352078&ev=PageView&noscript=1`}
+        />
+      </noscript>
       </body>
     </html>
   );
