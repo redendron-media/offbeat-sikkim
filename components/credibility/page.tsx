2077d29e7d1d3afb1d188f60bbd1f3e02ac85ca8
Ramit <ramitsingh12@gmail.com>
2024-09-02 13:43:39 +0530

    credibility marker change

--- components/credibility/page.tsx
+++ components/credibility/page.tsx
@@ -8,21 +8,31 @@ const Credibility = () => {
       <h2 className="headlines md:headlinem lg:headlinel text-secondary-oncontainer text-center">
         We are affliated with
       </h2>
-      <div className="flex flex-row flex-wrap justify-center gap-12 md:gap-8 lg:gap-14 xl:gap-20 items-center">
-        <Link href={"https://msme.gov.in/"} target="_blank">
-          {" "}
-          <Image src="/icons/msme.png" alt={"MSME"} width={200} height={200} />
-        </Link>
-        <Link href={"https://sikkimtourism.gov.in/Public/index"} target="_blank">
-          {" "}
-          <Image
-            src="/icons/sikkimtourismlogo.png"
-            alt={"Sikkim Tourism"}
-            width={200}
-            height={200}
-          />
-        </Link>
+      <div className="flex flex-row  justify-center gap-12 md:gap-8 lg:gap-14 xl:gap-20 items-start">
+    <Link href="https://msme.gov.in/" target="_blank">
+      <div className="flex flex-col items-center text-center">
+        <Image src="/icons/msme.png" alt="MSME" width={200} height={200} />
+        <div className="h-16 flex flex-col justify-center">
+          <p className="labels md:labell">Ministry of Micro, Small & Medium</p>
+          <p className="labels md:labell">  Enterprises</p>
+        </div>
       </div>
+    </Link>
+    <Link href="https://sikkimtourism.gov.in/Public/index" target="_blank">
+      <div className="flex flex-col items-center text-center">
+        <Image
+          src="/icons/sikkimtourismlogo.png"
+          alt="Sikkim Tourism"
+          width={200}
+          height={200}
+        />
+        <div className="h-16 flex flex-col justify-center">
+          <p className="labels md:labell">Tourism Department,</p>
+          <p className="labels md:labell">Govt. Of Sikkim</p>
+        </div>
+      </div>
+    </Link>
+  </div>
     </div>
   );
 };
