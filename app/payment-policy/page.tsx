df1bdab285fc00a37ff9dd19531e55d9c6c76476
Ramit <ramitsingh12@gmail.com>
2025-01-09 11:24:50 +0530

    Payment policy

---
+++ app/payment-policy/page.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const PaymentPolicy = () => {
+  return (
+    <main className="px-4 md:px-6 bg-[#F6FBF4] py-12  pt-20 md:pt-32 lg:py-36 max-w-screen-2xl mx-auto flex flex-col gap-6">
+      <h1 className="text-secondary-oncontainer headlines md:displays lg:displaym">
+       Pay us at
+      </h1>
+    
+
+      <div className=" bg-[#E4EAE3] w-full scroll-mt-56 rounded-xl flex flex-col gap-2 p-6">
+        <h2 className="titlel text-[#171D19] pb-1">Bank Transfer</h2>
+        <p className="text-black bodym md:bodyl">
+         <span className="font-bold">A/C No:</span> 531501010035344
+        </p>
+        <p className="text-black bodym md:bodyl">
+         <span className="font-bold">A/C Name:</span> Offbeatsikkim Travels
+
+        </p>
+        <p className="text-black bodym md:bodyl">
+         <span className="font-bold">IIFC Code: </span>UBIN0553158
+        </p>
+      </div>
+
+      <div className=" bg-[#E4EAE3] w-full scroll-mt-56 rounded-xl flex flex-col gap-2 p-6">
+        <h2 className="titlel text-[#171D19] pb-1">UPI</h2>
+        <p className="text-black bodym md:bodyl">
+         <span className="font-bold">UPI ID: </span> 8597669309@ibl
+        </p>
+        <p className="text-black bodym md:bodyl">
+         <span className="font-bold">UPI Name:</span> OFFBEAT SIKKIM
+        </p>
+    
+      </div>
+
+      <h4 className="titlel">Payment Policy</h4>
+      <ul className="space-y-3 list-disc list-outside pl-8">
+        <li className="text-black bodym md:bodyl">
+        To confirm your booking, a deposit of INR5000 Per Head is required in advance.
+        </li>
+        <li className="text-black bodym md:bodyl">
+        The remaining amount must be paid at least 2 days before the trip departure.
+        </li>
+        <li className="text-black bodym md:bodyl">
+          No Refund shall be made with respect to the initial booking amount for
+          any cancellations. However, if cancellations are made 30 days before
+          the start date of the trip, 50% of the trip cost will be charged as
+          cancellation fees.
+        </li>
+        <li className="text-black bodym md:bodyl">
+          If cancellations are made 15-30 days before the start date of the
+          trip, 75% of the trip cost will be charged as cancellation fees.
+        </li>
+        <li className="text-black bodym md:bodyl">
+          If cancellations are made within 0-15 days before the start date of
+          the trip, 100% of the trip cost will be charged as cancellation fees.
+        </li>
+        <li className="text-black bodym md:bodyl">
+          The refund will be processed within 48 hours.
+        </li>
+        <li className="text-black bodym md:bodyl">
+          In the case of unforeseen weather conditions or government
+          restrictions, certain activities may be cancelled and in such cases,
+          the operator will try their best to provide an alternate feasible
+          activity. However, no refund shall be provided for the same.
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default PaymentPolicy;
--- components/credibility/page.tsx
+++ components/credibility/page.tsx
@@ -10,7 +10,7 @@ const Credibility = () => {
       </h2>
       <div className="flex flex-row  justify-center gap-12 md:gap-8 lg:gap-14 xl:gap-20 items-start">
         <Link href="https://msme.gov.in/" target="_blank">
-          <div className="flex flex-col items-center text-center">
+          <div className="flex flex-col gap-3 md:gap-0 items-center text-center">
             <Image src="/icons/msme.png" alt="MSME" width={200} height={200} />
             <div className="h-16 flex flex-col justify-center">
               <p className="labels md:labell">
@@ -34,17 +34,31 @@ const Credibility = () => {
             </div>
           </div>
         </Link>
+
+        <div className="flex flex-col items-center text-center">
+          <Image
+            src="/icons/taas.png"
+            alt="Sikkim Tourism"
+            width={150}
+            height={150}
+            className="object-cover"
+          />
+          <div className="h-16 flex flex-col justify-center">
+            <p className="labels md:labell">
+              Travel Agents' Association of Sikkim
+            </p>
+            {/* <p className="labels md:labell">Govt. Of Sikkim</p> */}
+          </div>
+        </div>
       </div>
       <h2 className="headlines md:headlinem lg:headlinel text-secondary-oncontainer text-center">
         Trusted By
       </h2>
-      <Link href="https://ntpc.co.in/" target="_blank">
+      <Link className="w-fit mx-auto" href="https://ntpc.co.in/" target="_blank">
         <div className="flex flex-col items-center text-center">
           <Image src="/icons/ntpc.png" alt="NTPC" width={200} height={200} />
           <div className="h-16 flex flex-col justify-center">
-            <p className="labels md:labell">
-              NTPC PVT LTD.
-            </p>
+            <p className="labels md:labell">NTPC PVT LTD.</p>
           </div>
         </div>
       </Link>
